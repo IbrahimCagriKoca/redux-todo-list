684bf9c592a60e2c8d96f71b58768e1f0a3a8ab6
Çağrı Koca <52669161+IbrahimCagriKoca@users.noreply.github.com>
2020-10-03 19:57:05 +0300

    Update todoSlice.js

--- src/features/todo/todoSlice.js
+++ src/features/todo/todoSlice.js
@@ -54,44 +54,9 @@ const getFiltered = (todos, filter) => {
     }
 }
 
-/*
-Burada select Todo stateden state.todo objesinin icindekileri donuyor
-statein bir parcasini yani
-ama bizim ekrana basarken eger bir filtre secilmisse
-todolar arasindan filtrelenmis olan bir kismini basmamiz gerekiyor
-const { todo: { todos, filter } } = state;
-bunu yaptigimizda statede tuttugumuz butun todolari(todos) ve filtremizi("ALL", "COMPLETED" veya "ACTIVE")
-aliyoruz elimize
-sonra selectora dondugumuz deger state.todo icindeki hersey 
-+ 
-todos'un filtrelenmis hali bakiniz getFiltered
-*/
 export const selectTodo = state => {
     const { todo: { todos, filter } } = state;
     return { ...state.todo, filtered: getFiltered(todos, filter) }
 }
-// yukarıda ekledğini çekiyor değil mi
 
-// degisken degil kanka objenin bir propertysi
-// degisken gibi dusunebilirsin ama 
-// console log'a baktin mi
-/*
-
-{todos: Array(3), filter: "ACTIVE", filtered: Array(1)}
-
-filter: "ACTIVE"               filter aktifler
-
-filtered: Array(1)             filtered sadece aktif olanlar (getFiltereddan gelenler)
-0: {id: "8oSHRT3xq8pqHbI3mazGc", text: "asdasd", completed: false}
-
-todos: Array(3)                todos da butun hepsi
-0: {id: "8oSHRT3xq8pqHbI3mazGc", text: "asdasd", completed: false}
-1: {id: "PuggSnHGPWei5YRQCeM9X", text: "qwewq", completed: true}
-2: {id: "Z3SUETc0Bira-iiBVye0Z", text: "qweqw", completed: true}
-*/
-
-/* 
-sen bir butona bastiginda sadece statede tuttugun 'filter' propertysi degisiyor
-getFiltered da ona gore filtreledigi icin aslinda filteredi degistirmis oluyosun
-*/
 export default todoSlice.reducer;
